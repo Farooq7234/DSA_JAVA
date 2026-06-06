c79a131674af4069c8bcc8c41959a8d091820c3d
farooqxcodes <umarfarooq73970@gmail.com>
2026-06-06 16:42:13 +0530

    first commit

---
+++ loops.java
@@ -0,0 +1,12 @@
+// For every first loop i the inner loop j will run n times
+
+public class loop {
+    public static void main(String[] args) {
+        for(int i = 0; i <5 ; i++){
+            for(int j= 0; j < 5 ; j++){
+                System.out.print(" * ");
+            }
+            System.out.println(" "); // it will take one line space
+        }
+    }
+}
