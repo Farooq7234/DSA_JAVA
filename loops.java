a8dea778001b3e4912e888f9bd2f1ca1350fd079
farooqxcodes <umarfarooq73970@gmail.com>
2026-06-06 17:03:53 +0530

    loops added

--- loops.java
+++ loops.java
@@ -2,11 +2,48 @@
 
 public class loops {
     public static void main(String[] args) {
-        for(int i = 0; i <5 ; i++){
-            for(int j= 0; j < 5 ; j++){
-                System.out.print(" * ");
-            }
-            System.out.println(" "); // it will take one line space
-        }
+
+        // NESTED LOOP IN JAVA
+
+        // for(int i = 0; i <5 ; i++){
+        //     for(int j= 0; j < 5 ; j++){
+        //         System.out.print(" * ");
+        //     }
+        //     System.out.println(" "); // it will take one line space
+        // }
+
+        // BREAK IN JAVA
+
+        // for(int i =  0; i <= 10; i++){
+        //     if(i ==8){
+        //         break;
+        //     }
+        //     System.out.println(i);
+        // }
+
+        // CONTINUE IN JAVA
+
+        // for(int i =  0; i <= 10; i++){
+        //     if(i ==8){
+        //         continue;
+        //     }
+        //     System.out.println(i);
+        // }
+
+        // WHILE LOOP IN JAVA 
+
+        // int i = 1;
+        // while (i<=5){
+        //     System.out.println("i is " + i);
+        //     i++;
+        // }
+
+
+        // DO WHILE LOOP 
+
+        // int i = 0;
+        // do { 
+        //     System.out.println("Hi");
+        // } while (i > 1);
     }
 }
