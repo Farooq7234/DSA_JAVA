01b154880f6bc25b888b97495da4167610bb5b34
farooqxcodes <umarfarooq73970@gmail.com>
2026-06-06 16:42:43 +0530

    change

--- loops.java
+++ loops.java
@@ -1,6 +1,6 @@
 // For every first loop i the inner loop j will run n times
 
-public class loop {
+public class loops {
     public static void main(String[] args) {
         for(int i = 0; i <5 ; i++){
             for(int j= 0; j < 5 ; j++){
